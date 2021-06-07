e82bf9e14aaf2d312401dbb0cf52df4b79a75468
Blessing <bibiebabajide@gmail.com>
2021-06-07 17:21:12 +0000

    Change skills click listener to scroll listener.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,15 +15,56 @@
 /**
  * Adds a random greeting to the page.
  */
- function addRandomGreeting() {
-    const greetings =
-        ['Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.', 'There’s a lot of beauty in ordinary things. Isn’t that kind of the point?', 'Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.', 'I guess I’ve been working so hard, I forgot what it’s like to be hardly working.', 'I am Beyonce, always.'];
-  
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+        var top_of_element = $(".chart-wrapper").offset().top;
+        var bottom_of_element = $(".chart-wrapper").offset().top + $(".chart-wrapper").outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
+
+        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+            // the element is visible, do something
+            $('.chart-wrapper').addClass("in-view");
+        } else {
+            // the element is not visible, do something else
+            $('.chart-wrapper').removeClass("in-view");
+        }
+    })
+    //for the menubar
+    $('.menu-bar').click(function () {
+        $('.navbar .links').toggleClass("active");
+        $('.menu-bar i').toggleClass("active");
+    })
+    //for the project links
+    $('.chart-wrapper').click(function () {
+        $('.chart-wrapper').addClass("in-view");
+
+    })
+    $("#gh").on('click', function (event) {
+        event.preventDefault();
+        window.open("https://github.com/bibiebliss?tab=repositories", "_blank");
+    })
+    $("#cp").on('click', function (event) {
+        event.preventDefault();
+        window.open("https://codepen.io/bibiebliss", "_blank");
+    })
+});
+
+
+
+function addRandomGreeting() {
+    const greetings = ['Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.', 'There’s a lot of beauty in ordinary things. Isn’t that kind of the point?', 'Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.', 'I guess I’ve been working so hard, I forgot what it’s like to be hardly working.', 'I am Beyonce, always.'];
+
     // Pick a random greeting.
     const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-  
+
     // Add it to the page.
     const greetingContainer = document.getElementById('greeting-container');
     greetingContainer.innerText = greeting;
-  }
-  
+}
