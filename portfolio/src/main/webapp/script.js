3a3efa8639212899fbc7a46b9d92dddea39443c1
Blessing <bibiebabajide@gmail.com>
2021-06-18 17:22:15 +0000

    Add hard coded chart to page.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -56,6 +56,33 @@
     })
 });
 
+
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+/** Creates a chart and adds it to the page. */
+function drawChart() {
+  const data = new google.visualization.DataTable();
+  data.addColumn('string', 'Animal');
+  data.addColumn('number', 'Count');
+        data.addRows([
+          ['Research', 30],
+          ['Tutoring', 50],
+          ['Technical', 20]
+        ]);
+
+  const options = {
+    'title': 'Experience Chart',
+    'width':500,
+    'height':400
+  };
+
+  const chart = new google.visualization.PieChart(
+      document.getElementById('chart-container'));
+  chart.draw(data, options);
+}
+
 async function showContent() {
     const response = await fetch("/hello");
     const responseText = await response.json();
